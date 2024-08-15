c011635f01982b971cd6c792adbe5caba05a54e5
Karippery, John Raphy <john.raphy.karippery@edag.com>
2024-08-15 18:30:05 +0200

    fix admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from user.models import User
 # Register your models here.
+class UserAdmin(User):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+admin.site.register(User)
--- user/models.py
+++ user/models.py
@@ -36,6 +36,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_verified = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
